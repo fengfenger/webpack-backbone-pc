470e38621964b423289357a3344c22245d2b67b5
xifeng.cao <xifeng.cao@yinyuetai.com>
2016-07-07 17:02:52 +0800

    测试backbkone

--- app/src/index.js
+++ app/src/index.js
@@ -7,5 +7,19 @@ $(function(){
     $("#indexContainer").html('这是我的第一个页面');
 });
 
+var base = require('base-extend-backbone');
 
+var ItemView = Backbone.View.extend({
+  tagName: 'li'
+});
+
+var BodyView = Backbone.View.extend({
+  el: 'body'
+});
+
+var item = new ItemView();
+var body = new BodyView();
+
+console.log(item.el);
+console.log(body.el);
 
